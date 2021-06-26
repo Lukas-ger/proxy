9b4ca2a8d86183345cce17a006ef9b81627033bd
Flamex <acc@flamex.dev>
2021-06-26 11:43:46 +0200

    added websocket support

--- index.js
+++ index.js
@@ -2,26 +2,33 @@ const http = require('http')
 const httpProxy = require('http-proxy')
 const fs = require("fs")
 
-const proxy = httpProxy.createServer({
+const proxy = httpProxy.createProxyServer({
   proxyTimeout: 30000,
-  timeout: 30000
+  timeout: 30000,
+  ws: true
 }).listen(3001)
 
-proxy.on('error', function (err, req, res) {
-  res.writeHead(500, {
-    'Content-Type': 'text/html'
-  })
+// proxy.on('error', function (err, req, res) {
+//   res.writeHead(500, {
+//     'Content-Type': 'text/html'
+//   })
 
-  res.end(`Something went wrong.<br><code>${err.message}</code>`)
-})
+//   res.end(`Something went wrong.<br><code>${err.message}</code>`)
+// })
 
-http.createServer(async function(req, res) {
+http.createServer(async function(req, res, head) {
   const target = JSON.parse(fs.readFileSync(`${__dirname}/config.json`, "utf-8"))[req.headers.host]
 
   switch (target?.type) {
     case "WEB":
       proxy.web(req, res, {
         target: `http://localhost:${target.target}`
+      })
+    break
+
+    case "WS":
+      proxy.ws(req, res.socket, head, {
+        target: `ws://localhost:${target.target}`
       })
     break
 
