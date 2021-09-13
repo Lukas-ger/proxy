c14482d46c2ed788e5ac582abac19d615c134a87
Flamex <acc@flamex.dev>
2021-09-13 20:18:50 +0200

    redo / "better" folder stucture

--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
 const http = require('http')
 const httpProxy = require('http-proxy')
 const fs = require("fs")
+const { greenBright } = require("chalk")
 
 const proxy = httpProxy.createProxyServer({
   proxyTimeout: 30000,
   timeout: 30000,
   ws: true
-}).listen(3001)
+}).listen(81)
 
 proxy.on('error', function (err, req, res) {
-  res.writeHead(500, {
-    'Content-Type': 'text/html'
-  })
-
-  res.end(`Something went wrong.<br><code>${err.message}</code>`)
+  res.writeHead(500)
+  res.end()
 })
 
 http.createServer(async function(req, res, head) {
@@ -22,30 +20,26 @@ http.createServer(async function(req, res, head) {
   switch (target?.type) {
     case "WEB":
       proxy.web(req, res, {
-        target: `http://localhost:${target.target}`
+        target: `http://127.0.0.1:${target.target}`
       })
     break
 
     case "WS":
       proxy.ws(req, res.socket, head, {
-        target: `ws://localhost:${target.target}`
+        target: `ws://127.0.0.1:${target.target}`
       })
     break
 
     case "REDIRECT":
       res.writeHead(302, {
-        'Location': target.keepPath ? target.target + req.url : target.target
+        'Location': target.target
       })
       res.end()
     break
 
     default:
-      res.writeHead(404, {
-        'Content-Type': 'text/html'
-      })
-      res.end("Something went wrong.<br><code>Page not found</code>")
+      res.writeHead(404)
+      res.end()
     break
   }
-}).listen(80, () => {
-  console.log(`${new Date()} | Proxy started`)
-})
+}).listen(80, () => console.log(greenBright("Running on post 80")))
