510c390ee37f51b831790a15122a8fc103b8be72
Flamex <contact@flamex.dev>
2022-01-16 12:32:43 +0100

    import .js files with extenstion

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 const http = require('http')
 const fs = require("fs")
 const { greenBright } = require("chalk")
-const { ports, pattern } = require("./config")
-const proxyServer = require("./handler/proxyServer")
-const onError = require("./handler/onError")
+const { ports, pattern } = require("./config.js")
+const proxyServer = require("./handler/proxyServer.js")
+const onError = require("./handler/onError.js")
 
 // Check for valid ports
 if (!pattern.ports.test(ports.proxyServer)) throw new TypeError('The proxyServer port needs to be type of "number"')
